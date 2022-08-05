a9b0caea3a8b9921b3f069c31b238110d45e033b
YongHun Byun <yonghunbyun@gmail.com>
2022-08-05 19:34:52 +0900

    Add fade option

--- ekko-lightbox.js
+++ ekko-lightbox.js
@@ -15,6 +15,7 @@ const Lightbox = (($) => {
 		maxHeight: 9999,
 		containerClass: '',
 		imageClass: 'img-fluid',
+		fade: true, //if true, use fade effect
 		showArrows: true, //display the left / right arrows or not
 		wrapping: true, //if true, gallery loops infinitely
 		type: null, //force the lightbox into image / youtube mode. if null, or not image|youtube|vimeo; detect it
@@ -97,7 +98,8 @@ const Lightbox = (($) => {
 			let footer = `<div class="modal-footer${this._config.footer ? '' : ' hide'}">${this._config.footer || "&nbsp;"}</div>`;
 			let body = `<div class="modal-body">${this._config.title ? '' : btn}<div class="ekko-lightbox-container"><div class="ekko-lightbox-item fade in show"></div><div class="ekko-lightbox-item fade"></div></div></div>`
 			let dialog = `<div class="modal-dialog" role="document"><div class="modal-content">${header}${body}${footer}</div></div>`
-			$(this._config.doc.body).append(`<div id="${this._modalId}" class="ekko-lightbox ${this._config.containerClass} modal fade" tabindex="-1" tabindex="-1" role="dialog" aria-hidden="true">${dialog}</div>`)
+			let fade = this._$element.data('fade') === 'false' ? false : this._config.fade;
+			$(this._config.doc.body).append(`<div id="${this._modalId}" class="ekko-lightbox ${this._config.containerClass} modal` + (fade ? ' fade' : '') + `" tabindex="-1" tabindex="-1" role="dialog" aria-hidden="true">${dialog}</div>`)
 
 			this._$modal = $(`#${this._modalId}`, this._config.doc)
 			this._$modalDialog = this._$modal.find('.modal-dialog').first()
